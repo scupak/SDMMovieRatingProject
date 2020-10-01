105c240d50ac865da38f8c24591a8e4b234c34c7
scupak <anto1095@easv365.dk>
2020-10-01 14:49:19 +0200

    opgaven

--- MovieRating.Core/ApplicationService/Impl/MovieRatingService.cs
+++ MovieRating.Core/ApplicationService/Impl/MovieRatingService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using MovieRating.Core.DomainService;
 using MovieRating.Entity;
@@ -14,60 +15,196 @@ namespace MovieRating.Core.ApplicationService.Impl
       {
           RatingRepo = ratingRepo ?? throw new ArgumentException("Missing RatingRepo");
       }
+      // 1. On input N, what are the number of reviews from reviewer N?
+        public int GetNumberOfReviewsFromReviewer(int reviewer)
+      {
+          return RatingRepo.GetAll().Where((r => r.Reviewer == reviewer)).Count();
+      }
 
-      public int GetNumberOfReviewsFromReviewer(int reviewer)
-        {
-            throw new NotImplementedException();
-        }
-
+        // 2. On input N, what is the average rate that reviewer N had given?
         public double GetAverageRateFromReviewer(int reviewer)
         {
-            throw new NotImplementedException();
+
+            if (RatingRepo.GetAll().Count == 0)
+            {
+                throw new ArgumentException("List is empty");
+
+            }
+
+            List<Rating> ratingsfromReviewer = RatingRepo.GetAll().Where(rating => rating.Reviewer == reviewer).ToList();
+
+            double sumRating = 0;
+
+            foreach (Rating rating in ratingsfromReviewer)
+            {
+
+                sumRating += rating.Grade;
+
+
+            }
+
+            
+
+
+            double AverageRateFromReviewer = sumRating / ratingsfromReviewer.Count;
+
+            return AverageRateFromReviewer;
+
+
         }
 
+        // 3. On input N and R, how many times has reviewer N given rate R?
         public int GetNumberOfRatesByReviewer(int reviewer, int rate)
         {
-            throw new NotImplementedException();
+            List<Rating> ratingsfromReviewer = RatingRepo.GetAll().Where(rating => rating.Reviewer == reviewer).ToList();
+
+            int numberOfRates = 0;
+
+            foreach (Rating rating in ratingsfromReviewer)
+            {
+                if (rating.Grade == rate)
+                {
+                    numberOfRates++;
+                }
+
+
+
+            }
+
+            return numberOfRates;
+
         }
 
+        // 4. On input N, how many have reviewed movie N?
         public int GetNumberOfReviews(int movie)
         {
-            throw new NotImplementedException();
+
+            List<Rating> ratingsForMovie = RatingRepo.GetAll().Where(rating => rating.Movie == movie).ToList();
+
+            
+            return ratingsForMovie.Count;
+
         }
 
+        // 5. On input N, what is the average rate the movie N had received?
         public double GetAverageRateOfMovie(int movie)
         {
-            throw new NotImplementedException();
-        }
+            if (RatingRepo.GetAll().Count == 0)
+            {
+                throw new ArgumentException("List is empty");
 
+            }
+
+            List<Rating> ratingsForMovie = RatingRepo.GetAll().Where(rating => rating.Movie == movie).ToList();
+
+            double sumRating = 0;
+
+            foreach (Rating rating in ratingsForMovie)
+            {
+
+                sumRating += rating.Grade;
+
+
+            }
+
+
+
+
+            double AverageRateFromReviewer = sumRating / ratingsForMovie.Count;
+
+            return AverageRateFromReviewer;
+        }
+        // 6. On input N and R, how many times had movie N received rate R?
         public int GetNumberOfRates(int movie, int rate)
         {
-            throw new NotImplementedException();
+            List<Rating> ratingsForMovie = RatingRepo.GetAll().Where(rating => rating.Movie == movie).ToList();
+
+            int numberOfRates = 0;
+
+            foreach (Rating rating in ratingsForMovie)
+            {
+                if (rating.Grade == rate)
+                {
+                    numberOfRates++;
+                }
+
+
+
+            }
+
+            return numberOfRates;
         }
 
+        // 7. What is the id(s) of the movie(s) with the highest number of top rates (5)?
         public List<int> GetMoviesWithHighestNumberOfTopRates()
         {
-            throw new NotImplementedException();
+            var movie5 = RatingRepo.GetAll()
+                .Where(r => r.Grade == 5)
+                .GroupBy(r => r.Movie)
+                .Select(group => new {
+                Movie = group.Key,
+                MovieGrade5 = group.Count()
+            });
+
+
+            int max5 = movie5.Max(grp => grp.MovieGrade5);
+
+            return movie5.Where(grp => grp.MovieGrade5 == max5).Select(grp => grp.Movie).ToList();
         }
 
+        // 8. What reviewer(s) had done most reviews?
         public List<int> GetMostProductiveReviewers()
         {
-            throw new NotImplementedException();
+            var reviewers = RatingRepo.GetAll()
+                .GroupBy(r => r.Reviewer)
+                .Select(group => new {
+                    Reviewer = group.Key,
+                    Reviewercount = group.Count()
+                });
+
+            int max5 = reviewers.Max(grp => grp.Reviewercount);
+
+            return reviewers.Where(grp => grp.Reviewercount == max5).Select(grp => grp.Reviewer).ToList();
+
         }
 
+        // 9. On input N, what is top N of movies? The score of a movie is its average rate.
         public List<int> GetTopRatedMovies(int amount)
         {
-            throw new NotImplementedException();
+
+            return RatingRepo.GetAll()
+                .GroupBy(r => r.Movie)
+                .Select(grp => new
+                {
+                    Movie = grp.Key,
+                    GradeAvg = grp.Average(x => x.Grade)
+                })
+                .OrderByDescending(grp => grp.GradeAvg)
+                .OrderByDescending(grp => grp.GradeAvg)
+                .Select(grp => grp.Movie)
+                .Take(amount)
+                .ToList();
+
         }
 
+        // 10. On input N, what are the movies that reviewer N has reviewed? The list should be sorted decreasing by rate first, and date secondly.
         public List<int> GetTopMoviesByReviewer(int reviewer)
         {
-            throw new NotImplementedException();
+            List<Rating> ratingsfromReviewer = RatingRepo.GetAll().Where(rating => rating.Reviewer == reviewer).ToList();
+
+            return ratingsfromReviewer.OrderByDescending(rating => rating.Grade).ThenByDescending(rating => rating.Date)
+                .Select(rating => rating.Movie).ToList();
+
+
         }
 
+        // 11. On input N, who are the reviewers that have reviewed movie N? The list should be sorted decreasing by rate first, and date secondly.
         public List<int> GetReviewersByMovie(int movie)
         {
-            throw new NotImplementedException();
+            List<Rating> ratingsForMovie = RatingRepo.GetAll().Where(rating => rating.Movie == movie).ToList();
+
+            return ratingsForMovie.OrderByDescending(rating => rating.Grade).ThenByDescending(rating => rating.Date)
+                .Select(rating => rating.Reviewer).ToList();
         }
     }
 }
--- UnitTests/MovieRatingServiceTest.cs
+++ UnitTests/MovieRatingServiceTest.cs
@@ -60,6 +60,7 @@ namespace UnitTests
         [Theory]
         [InlineData(1,4)]
         [InlineData(2, 2)]
+        [InlineData(3, 1)]
         public void GetNumberOfReviewsFromReviewerTest(int reviewerid, int expected)
         {
             //arrange
@@ -93,6 +94,8 @@ namespace UnitTests
         // 2. On input N, what is the average rate that reviewer N had given?
         [Theory]
         [InlineData(1,2.75)]
+        [InlineData(2, 3.5)]
+        [InlineData(6, 3)]
         public void GetAverageRateFromReviewer(int reviewer, double expected)
         {
             //arrange
@@ -109,6 +112,8 @@ namespace UnitTests
                 new Rating(4, 5, 2, new DateTime(2005,3,23)),
                 new Rating(5, 5, 2, new DateTime(2005,4,23)),
                 new Rating(6, 5, 2, new DateTime(2005,5,23)),
+                new Rating(6, 5, 5, new DateTime(2005,5,23)),
+                new Rating(6, 5, 2, new DateTime(2005,5,23)),
                 new Rating(2,2,3,new DateTime(2002,1,22)),
                 new Rating(2,3,4,new DateTime(2001,12,1))
 
@@ -117,10 +122,42 @@ namespace UnitTests
             //act
             double averageRateFromReviewer = mrs.GetAverageRateFromReviewer(reviewer);
 
-            //assert
-            Assert.True(Math.Abs(expected - averageRateFromReviewer) < 0);
+            _testOutputHelper.WriteLine(averageRateFromReviewer.ToString());
 
-            repoMock.Verify(rep => rep.GetAll(),Times.Once);
+            //assert
+            Assert.Equal(expected,averageRateFromReviewer);
+
+            repoMock.Verify(rep => rep.GetAll(),Times.AtLeastOnce);
+
+
+        }
+
+        // 2. On input N, what is the average rate that reviewer N had given Expect exeption?
+        [Fact]
+        public void GetAverageRateFromReviewerArgumentException()
+        {
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+
+            dataStore = new List<Rating>
+            {
+                
+
+            };
+
+            // act
+            var ex = Assert.Throws<ArgumentException>(() =>
+            {
+                double AverageRateFromReviewer = mrs.GetAverageRateFromReviewer(1);
+            });
+
+
+            //assert
+            // assert
+            Assert.Equal("List is empty", ex.Message);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
 
 
         }
@@ -128,6 +165,8 @@ namespace UnitTests
         // 3. On input N and R, how many times has reviewer N given rate R?
         [Theory]
         [InlineData(1,2,2)]
+        [InlineData(2, 2, 0)]
+        [InlineData(5, 2, 1)]
         public void GetNumberOfRatesByReviewer(int reviewer, int rate,int expected)
         {
             //arrange
@@ -156,12 +195,16 @@ namespace UnitTests
             //assert 
             Assert.Equal(expected,numberOfRatesByReviewer);
 
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
+
 
         }
 
         // 4. On input N, how many have reviewed movie N?
         [Theory]
         [InlineData(1,1)]
+        [InlineData(3, 2)]
+        [InlineData(5, 4)]
         public void GetNumberOfReviews(int movie, int expected)
         {
 
@@ -190,12 +233,16 @@ namespace UnitTests
             //assert 
             Assert.Equal(expected, NumberOfReviews);
 
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
 
         }
 
         // 5. On input N, what is the average rate the movie N had received?
         [Theory]
         [InlineData(1,2.5)]
+        [InlineData(3, 3.5)]
+        [InlineData(4, 4)]
+        [InlineData(5, 2.5)]
         public void GetAverageRateOfMovie(int movie, double expected)
         {
             //arrange
@@ -204,9 +251,10 @@ namespace UnitTests
             dataStore = new List<Rating>
             {
                 new Rating(1, 1, 2, new DateTime(2003, 6, 6)),
-                new Rating(1, 3, 3, new DateTime(2005, 9, 6)),
-                new Rating(1, 4, 4, new DateTime(2004, 12, 23)),
-                new Rating(1, 6, 2, new DateTime(2005, 1, 23)),
+                new Rating(10, 1, 3, new DateTime(2003, 6, 6)),
+                new Rating(2, 3, 3, new DateTime(2005, 9, 6)),
+                new Rating(3, 4, 4, new DateTime(2004, 12, 23)),
+                new Rating(4, 6, 2, new DateTime(2005, 1, 23)),
                 new Rating(3, 5, 2, new DateTime(2005, 2, 23)),
                 new Rating(4, 5, 2, new DateTime(2005, 3, 23)),
                 new Rating(5, 5, 3, new DateTime(2005, 4, 23)),
@@ -214,20 +262,54 @@ namespace UnitTests
                 new Rating(2, 2, 3, new DateTime(2002, 1, 22)),
                 new Rating(2, 3, 4, new DateTime(2001, 12, 1))
             };
-
+            
 
             //act 
-                double AverageRateOfMovie = mrs.GetAverageRateFromReviewer(movie);
+                double AverageRateOfMovie = mrs.GetAverageRateOfMovie(movie);
 
+                //assert
                 Assert.Equal(expected, AverageRateOfMovie);
 
+                repoMock.Verify(rep => rep.GetAll(), Times.AtLeastOnce);
 
-        
+
+        }
+
+        // 5. On input N, what is the average rate the movie N had received? Expect exeption,
+        [Fact]
+        public void GetAverageRateOfMovieExpectArgumentException()
+        {
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+            dataStore = new List<Rating>
+            {
+                
+            };
+
+
+            
+            // act
+            var ex = Assert.Throws<ArgumentException>(() =>
+            {
+                double AverageRateOfMovie = mrs.GetAverageRateOfMovie(1);
+            });
+
+
+            //assert
+            // assert
+            Assert.Equal("List is empty", ex.Message);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
+
+
         }
 
         // 6. On input N and R, how many times had movie N received rate R?
         [Theory]
-        [InlineData(1,1,1)]
+        [InlineData(1,2,1)]
+        [InlineData(5, 2, 2)]
+        [InlineData(6, 2, 2)]
         public void GetNumberOfRates(int movie, int rate, int expected)
         {
             //arrange
@@ -244,7 +326,8 @@ namespace UnitTests
                 new Rating(5, 5, 3, new DateTime(2005, 4, 23)),
                 new Rating(6, 5, 3, new DateTime(2005, 5, 23)),
                 new Rating(2, 2, 3, new DateTime(2002, 1, 22)),
-                new Rating(2, 3, 4, new DateTime(2001, 12, 1))
+                new Rating(2, 3, 4, new DateTime(2001, 12, 1)),
+                new Rating(8, 6, 2, new DateTime(2005, 1, 23)),
             };
 
 
@@ -252,39 +335,203 @@ namespace UnitTests
             int NumberOfRates = mrs.GetNumberOfRates(movie,rate);
 
             Assert.Equal(expected, NumberOfRates);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
         }
 
         // 7. What is the id(s) of the movie(s) with the highest number of top rates (5)?
         [Fact]
         public void GetMoviesWithHighestNumberOfTopRates()
         {
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+            dataStore = new List<Rating>
+            {
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(1, 3, 3, new DateTime(2005, 9, 6)),
+                new Rating(1, 4, 4, new DateTime(2004, 12, 23)),
+                new Rating(1, 6, 2, new DateTime(2005, 1, 23)),
+                new Rating(3, 5, 2, new DateTime(2005, 2, 23)),
+                new Rating(4, 5, 2, new DateTime(2005, 3, 23)),
+                new Rating(5, 5, 5, new DateTime(2005, 4, 23)),
+                new Rating(6, 5, 3, new DateTime(2005, 5, 23)),
+                new Rating(2, 2, 3, new DateTime(2002, 1, 22)),
+                new Rating(2, 3, 4, new DateTime(2001, 12, 1))
+            };
+
+            List<int> MoviesWithHighestNumberOfTopRatesExpected = new List<int>
+            {
+                1, 
+                5
+            };
+
+            //act 
+            List<int> MoviesWithHighestNumberOfTopRates = mrs.GetMoviesWithHighestNumberOfTopRates();
+
+            Assert.Equal(MoviesWithHighestNumberOfTopRatesExpected, MoviesWithHighestNumberOfTopRates);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
+
         }
 
         // 8. What reviewer(s) had done most reviews?
         [Fact]
         public void GetMostProductiveReviewers()
         {
+
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+            dataStore = new List<Rating>
+            {
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(1, 3, 3, new DateTime(2005, 9, 6)),
+                new Rating(1, 4, 4, new DateTime(2004, 12, 23)),
+                new Rating(1, 6, 2, new DateTime(2005, 1, 23)),
+                new Rating(3, 5, 2, new DateTime(2005, 2, 23)),
+                new Rating(4, 5, 2, new DateTime(2005, 3, 23)),
+                new Rating(5, 5, 5, new DateTime(2005, 4, 23)),
+                new Rating(6, 5, 3, new DateTime(2005, 5, 23)),
+                new Rating(2, 2, 3, new DateTime(2002, 1, 22)),
+                new Rating(2, 3, 4, new DateTime(2001, 12, 1)),
+                new Rating(7, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(7, 3, 3, new DateTime(2005, 9, 6)),
+                new Rating(7, 4, 4, new DateTime(2004, 12, 23)),
+                new Rating(7, 6, 2, new DateTime(2005, 1, 23))
+            };
+
+            List<int> MostProductiveReviewersExpected = new List<int>
+            {
+                1,
+                7
+            };
+
+
+            //act 
+            List<int> MostProductiveReviewers = mrs.GetMostProductiveReviewers();
+
+            Assert.Equal(MostProductiveReviewersExpected, MostProductiveReviewers);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
         }
 
         // 9. On input N, what is top N of movies? The score of a movie is its average rate.
+      
+
         [Theory]
-        [InlineData(1)]
-        public void GetTopRatedMovies(int amount)
+        [InlineData(1, new[] { 1 }) ]
+        [InlineData(2, new[] { 1,2 })]
+        [InlineData(3, new[] { 1, 2,3 })]
+        public void GetTopRatedMovies(int amount , int[] expected )
         {
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+            dataStore = new List<Rating>
+            {
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(1, 2, 4, new DateTime(2003, 6, 6)),
+                new Rating(1, 2, 4, new DateTime(2003, 6, 6)),
+                new Rating(1, 2, 4, new DateTime(2003, 6, 6)),
+                new Rating(1, 3, 4, new DateTime(2003, 6, 6)),
+                new Rating(1, 3, 4, new DateTime(2003, 6, 6)),
+                new Rating(1, 3, 4, new DateTime(2003, 6, 6)),
+
+            };
+
+            List<int> TopRatedMoviesExpected = new List<int>(expected);
+
+
+            //act 
+            List<int> MostProductiveReviewers = mrs.GetTopRatedMovies(amount);
+
+            //assert
+            Assert.Equal(TopRatedMoviesExpected, MostProductiveReviewers);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
         }
 
         // 10. On input N, what are the movies that reviewer N has reviewed? The list should be sorted decreasing by rate first, and date secondly.
         [Theory]
-        [InlineData(1)]
-        public void GetTopMoviesByReviewer(int reviewer)
+        [InlineData(1, new[] { 1, 2, 5, 4 ,3 })]
+        [InlineData(2, new[] { 2, 1, 4, 3 })]
+        public void GetTopMoviesByReviewer(int reviewer , int[] expected)
         {
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+            dataStore = new List<Rating>
+            {
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(1, 2, 4, new DateTime(2003, 6, 6)),
+                new Rating(1, 3, 3, new DateTime(2003, 6, 6)),
+                new Rating(1, 4, 3, new DateTime(2004, 6, 6)),
+                new Rating(1, 5, 3, new DateTime(2005, 6, 6)),
+
+
+                new Rating(2, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(2, 2, 5, new DateTime(2004, 6, 6)),
+                new Rating(2, 3, 3, new DateTime(2003, 6, 6)),
+                new Rating(2, 4, 4, new DateTime(2003, 6, 6)),
+               
+
+            };
+
+            List<int> TopRatedMoviesExpected = new List<int>(expected);
+
+
+            //act 
+            List<int> MostProductiveReviewers = mrs.GetTopMoviesByReviewer(reviewer);
+
+            //assert
+            Assert.Equal(TopRatedMoviesExpected, MostProductiveReviewers);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
+
         }
 
         // 11. On input N, who are the reviewers that have reviewed movie N? The list should be sorted decreasing by rate first, and date secondly.
         [Theory]
-        [InlineData(1)]
-        public void GetReviewersByMovie(int movie)
+        [InlineData(1 , new[] { 1, 2, 5, 4, 3 })]
+        [InlineData(2, new[] { 5, 4, 3, 2, 1 })]
+        public void GetReviewersByMovie(int movie, int[] expected)
         {
+            //arrange
+            IRepository<Rating> repo = repoMock.Object;
+            MovieRatingService mrs = new MovieRatingService(repo);
+            dataStore = new List<Rating>
+            {
+                new Rating(1, 1, 5, new DateTime(2003, 6, 6)),
+                new Rating(2, 1, 4, new DateTime(2003, 6, 6)),
+                new Rating(3, 1, 3, new DateTime(2003, 6, 6)),
+                new Rating(4, 1, 3, new DateTime(2004, 6, 6)),
+                new Rating(5, 1, 3, new DateTime(2005, 6, 6)),
+
+
+                new Rating(1, 2, 1, new DateTime(2003, 6, 6)),
+                new Rating(2, 2, 2, new DateTime(2003, 6, 6)),
+                new Rating(3, 2, 3, new DateTime(2003, 6, 6)),
+                new Rating(4, 2, 4, new DateTime(2004, 6, 6)),
+                new Rating(5, 2, 5, new DateTime(2005, 6, 6)),
+
+
+
+
+            };
+
+            List<int> ReviewersByMovieExpected = new List<int>(expected);
+
+
+            //act 
+            List<int> ReviewersByMovie = mrs.GetReviewersByMovie(movie);
+
+            //assert
+            Assert.Equal(ReviewersByMovieExpected, ReviewersByMovie);
+
+            repoMock.Verify(rep => rep.GetAll(), Times.Once);
         }
 
         private void GenericSetup()
